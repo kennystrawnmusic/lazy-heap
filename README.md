2d63b920679070cf9493990f00e12037db52da26
Kenny Strawn <Kenny.Strawn@gmail.com>
2024-03-29 19:36:52 -0700

    phil-opp/blog_os#1307: bump version and publish

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # `lazy_heap`: A wrapper around the `slab_allocator_rs` crate that allows for lazy initialization
 
-Although there are plenty of global allocators in the crates.io repository, 3 years of tinkering with my own kernel code have taught me that, sometimes, the more time saved, the better. As such, I came up with this in the code to my own kernel but decided, because of just how useful it really is, to actually open it up to the masses.
+Although there are plenty of global allocators in the crates.io repository, 3 years of tinkering with [my own kernel code](https://github.com/kennystrawnmusic/cryptos) have taught me that, sometimes, the more time saved, the better. As such, I came up with this in the code to my own kernel but decided, because of just how useful it really is, to actually open it up to the masses.
 
 Using this crate allows you to use a closure to initialize the heap automatically (lazily) on the first access attempt:
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+//! # `lazy_heap`: A wrapper around the `slab_allocator_rs` crate that allows for lazy initialization
+//!
+//! Although there are plenty of global allocators in the crates.io repository, 3 years of tinkering with [my own kernel code](https://github.com/kennystrawnmusic/cryptos) have taught me that, sometimes, the more time saved, the better. As such, I came up with this in the code to my own kernel but decided, because of just how useful it really is, to actually open it up to the masses.
+//!
+//! ## Usage
+//! Using this crate allows you to use a closure to initialize the heap automatically (lazily) on the first access attempt:
+//!
+//! ```rust
+//! use lazy_heap::LazyHeap;
+//! #[global_allocator]
+//! pub static ALLOC: LazyHeap = LazyHeap::new(|| {
+//!    // allocator initialization code goes here
+//! })
+//! ```
+//!
+//! This is a much more seamless, much less error-prone, set-it-and-forget-it way to initialize heap allocation than any other approach, because, with it, you can be guaranteed that any first attempt to use `alloc` will automatically initialize the heap for you.
+
 use core::alloc::{GlobalAlloc, Layout};
 use spin::Lazy;
 
